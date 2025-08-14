4a71e060c89e379dd19c54dfb15ad88a4b144b71
T.Miura <mtaiga0213@icloud.com>
2025-08-14 12:34:06 +0900

    rafactor: 型を分離

--- practices/lapras-api/app/actions.ts
+++ practices/lapras-api/app/actions.ts
@@ -1,48 +1,6 @@
 'use server'
 
-export interface LaprasProfile {
-  name: string
-  description: string
-  e_score: number
-  b_score: number
-  i_score: number
-  iconimage_url: string
-  qiita_articles: Array<{
-    title: string
-    url: string
-    tags: string[]
-    headlines: string[]
-    stockers_count: number
-    updated_at: string
-  }>
-  zenn_articles: Array<{
-    title: string
-    url: string
-    tags: string[]
-    posted_at: string
-  }>
-  github_repositories: Array<{
-    id: number
-    title: string
-    url: string
-    description: string
-    stargazers_count: number
-    language: string
-    languages: Array<{
-      name: string
-      bytes: number
-    }>
-    contributions: number
-  }>
-  events: Array<{
-    title: string
-    url: string
-    status: number
-    date: string
-    is_presenter: boolean
-    is_organizer: boolean
-  }>
-}
+import { LaprasProfile } from './types/lapras'
 
 export async function fetchLaprasProfile(): Promise<LaprasProfile> {
   const res = await fetch('https://lapras.com/public/W4MC1JR.json', {
