af8e1fa5070b0cdd237003b1f359260b1b1ca874
T.Miura <mtaiga0213@icloud.com>
2025-09-15 20:48:40 +0900

    feat: ライブラリの実装テスト

--- practices/wordcloud/app/page.tsx
+++ practices/wordcloud/app/page.tsx
@@ -1,103 +1,57 @@
-import Image from "next/image";
+"use client";
+import { Word, WordCloud } from "@isoterik/react-word-cloud";
 
-export default function Home() {
+const words: Word[] = [
+  {
+    text: "徹底的に我を消す | 薫る花は凛と咲く | チーフ",
+    value: 100
+  },
+  {
+    text: "行動基準は店のため | 薫る花は凛と咲く | チーフ",
+    value: 100
+  },
+  {
+    text: "道草を楽しめ 大いにな | HxH | zin",
+    value: 100
+  },
+  {
+    text: "少しだけ君のため生きてみますか | webkuso | life",
+    value: 100
+  },
+  {
+    text: "重厚なバックボーン | はじめの一歩 | ホークのセコンド",
+    value: 100
+  },
+  {
+    text: "ところで平凡な俺よ 下を向いている暇はあるのか | ハイキュー! | 田中龍之介",
+    value: 100
+  },
+  {
+    text: "プライド以外に何が要るんだ!! | ハイキュー! | 山口忠",
+    value: 100
+  },
+  {
+    text: "“ムリ”ではなく“ムズカシイ”である！！ | ハイキュー! | bokuto",
+    value: 100
+  },
+  {
+    text: "この恐怖が俺をここまで連れてきたんだ | フリーれん | ",
+    value: 100
+  },
+  {
+    text: "必要なものは覚悟だけだったのです。必死に積み上げてきたものは決して裏切りません。 |  | ",
+    value: 100
+  }
+];
+
+
+function App() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div
+    >
+      <WordCloud words={words} width={300} height={200} />
     </div>
   );
 }
+
+export default App;
