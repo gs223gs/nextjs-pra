5244ae9ae5a3c8e2698c8b3388b1647633afad97
T.Miura <mtaiga0213@icloud.com>
2025-09-16 21:07:40 +0900

    feat: 画像を取得するサイトのドメインを許可

--- practices/udemy-blog-auth/next.config.ts
+++ practices/udemy-blog-auth/next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [{ protocol: "https", hostname: "picsum.photos" }],
   },
-  /* config options here */
 };
 
 export default nextConfig;
