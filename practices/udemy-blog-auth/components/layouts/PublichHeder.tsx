ec933cd16cd9a6828d53f725c4365b2c079c8335
T.Miura <mtaiga0213@icloud.com>
2025-09-16 23:00:07 +0900

    feat: header の仮実装

---
+++ practices/udemy-blog-auth/components/layouts/PublichHeder.tsx
@@ -0,0 +1,48 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function PublichHeder() {
+  return (
+    <div>
+      <header className="border-b bg-blue-200">
+        <div className="container mx-aute px-4 py-4 flex items-center justify-between">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/">
+                  <NavigationMenuLink>home</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/posts">
+                  <NavigationMenuLink>Posts</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/login">
+                  <NavigationMenuLink>Login</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="flex items-center gap-4">
+            <Input type="text" placeholder="Search" />
+            <Button variant="outline" asChild>
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button variant="outline" asChild>
+              <Link href="/register">Sign up</Link>
+            </Button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
