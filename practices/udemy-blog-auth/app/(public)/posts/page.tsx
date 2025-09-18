9d100435ea304a4309750d4a3ba8cd2512318b9d
T.Miura <mtaiga0213@icloud.com>
2025-09-18 22:42:20 +0900

    feat: postを表示させるコンポーネントを実装

--- practices/udemy-blog-auth/app/(public)/posts/page.tsx
+++ practices/udemy-blog-auth/app/(public)/posts/page.tsx
@@ -1,9 +1,22 @@
+import { getPosts } from "@/lib/posts";
 
+export default async function pages() {
+  const posts = await getPosts();
 
-export default function pages() {
   return (
     <div>
       <h1>Posts</h1>
+      <ul className="space-y-4 p-4">
+        {posts.map((post) => (
+          <div key={post.id} className="border p-4 rounded-md m-2">
+            <li>title: {post.title}</li>
+            <li>name:{post.author.name}</li>
+            <li>content:{post.content}</li>
+            <li>published:{post.published}</li>
+            <li>authorId:{post.authorId}</li>
+          </div>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
