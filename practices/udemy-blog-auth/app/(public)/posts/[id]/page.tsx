672874ae53d767f932592225821852bb22aa2621
T.Miura <mtaiga0213@icloud.com>
2025-09-23 21:53:18 +0900

    feat: ポスト詳細ページのUIを改善し、存在しないポストに対する404エラーハンドリングを追加

--- practices/udemy-blog-auth/app/(public)/posts/[id]/page.tsx
+++ practices/udemy-blog-auth/app/(public)/posts/[id]/page.tsx
@@ -1,11 +1,35 @@
-import { getPost } from '@/lib/posts'
+import { getPost } from "@/lib/posts";
+import { notFound } from "next/navigation";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+
 export default async function pages({ params }: { params: { id: string } }) {
-  const post = await getPost(params.id)
+  const post = await getPost(params.id);
+  if (!post) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-      <p>{post.author.name}</p>
-    </div>
-  )
+    <Card>
+      <CardHeader>
+        <CardTitle>{post.title}</CardTitle>
+      </CardHeader>
+      {post.topImage && (
+        <Image src={post.topImage} alt={post.title} width={100} height={100} />
+      )}
+      <CardContent>
+        <p>{post.content}</p>
+      </CardContent>
+      <CardFooter>
+        <p>{post.author.name}</p>
+      </CardFooter>
+    </Card>
+  );
 }
