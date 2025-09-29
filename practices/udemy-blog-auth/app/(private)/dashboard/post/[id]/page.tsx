aa69e4e1e478952ab251c13ec28a946e0b37d4f7
T.Miura <mtaiga0213@icloud.com>
2025-09-29 23:00:43 +0900

    feat: 投稿詳細ページでDashBoardPostコンポーネントを使用して表示機能を改善

--- practices/udemy-blog-auth/app/(private)/dashboard/post/[id]/page.tsx
+++ practices/udemy-blog-auth/app/(private)/dashboard/post/[id]/page.tsx
@@ -1,15 +1,7 @@
 import { getPost } from "@/lib/posts";
 import { notFound } from "next/navigation";
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import DashBoardPost from "@/components/post/DashBoardPost";
 
 export default async function pages({ params }: { params: { id: string } }) {
   const { id } = await params;
@@ -18,22 +10,5 @@ export default async function pages({ params }: { params: { id: string } }) {
     notFound();
   }
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{post.title}</CardTitle>
-      </CardHeader>
-      {post.topImage && (
-        <Image src={post.topImage} alt={post.title} width={100} height={100} />
-      )}
-      <CardContent>
-        <p>{post.content}</p>
-      </CardContent>
-      <CardFooter>
-        <p>{post.author.name}</p>
-      </CardFooter>
-      <Button>編集</Button>
-      <Button>削除</Button>
-    </Card>
-  );
+  return <DashBoardPost post={post} />;
 }
